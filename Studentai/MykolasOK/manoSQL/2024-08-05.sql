97a089008172747a43279dd68d68a6a4c3afbb53
Mykolas OK <mykolasok@gmail.com>
2024-08-05 21:01:39 +0300

    SQL komandos mažosiomis.

--- Studentai/MykolasOK/manoSQL/2024-08-05.sql
+++ Studentai/MykolasOK/manoSQL/2024-08-05.sql
@@ -1,12 +1,12 @@
-select actor_id as ID,
-	CASE
+select actor_id as id,
+	case
 		when actor_id MOD 100 = 0 then 'Šimtinis'
 		when actor_id MOD 10 = 0 then 'Dešimtinis'
 		when actor_id MOD 2 = 0 then 'Lyginis'
 		else 'Nelyginis'
-	END AS Lyginumas, first_name, last_name 
+	end as Lyginumas, first_name, last_name 
 	from actor
-	order by ID asc;
+	order by id asc;
 	
 Kas antro aktoriaus vardą parašykite mažosiomis raidėmis, 
 kas trečio - pavardę.
@@ -28,10 +28,10 @@ select actor_id as id,
 	
 -- Iš lentelės rental išrinkite įrašus, kurių return_date būtų birželio mėnuo.
 
-select MONTH(return_date) from rental;
+select month(return_date) from rental;
 
 select * from rental
-	WHERE MONTH(return_date)=6;
+	where month(return_date)=6;
 
 -- Suskaičiuokite kiek klientų (lentelė customer) yra aktyvių ir kiek pasyvių. 
 -- Jei stulpelyje active yra reikšmė 1 - tai aktyvus klientas, o jei 0 - tai neaktyvus. 
@@ -44,37 +44,38 @@ select count(*),
 	end as aktyvumas
 	from customer group by active ;
 
-SELECT rating, GROUP_CONCAT(title separator ', ') 
+select rating, group_concat(title separator ', ') 
 	from film 
 	group by rating;
 
-SELECT SUBSTRING(first_name, 1, 3) from actor;
+select substring(first_name, 1, 3) from actor;
 
-SELECT first_name, last_name from actor a 
+select first_name, last_name from actor a 
 	union all
-SELECT first_name, last_name from customer c; 
+select first_name, last_name from customer c; 
 
-SELECT first_name, last_name, 'actor' from actor a 
+select first_name, last_name, 'actor' from actor a 
 	union all
-SELECT first_name, last_name, 'customer' from customer c; 
+select first_name, last_name, 'customer' from customer c; 
 
 -- Parašykite SQL užklausą, pateikiančią klientų id, sumokamą mokestį už nuomą. 
 -- Tuos klientus, kurie sumoka už nuomą vienu kartu virš 10, pažymėkite kaip „Virš 10“, 
 -- o išleidžiančius iki 10, pažymėkite „Iki 10“. Surūšiuokite pagal nuomos mokestį mažėjimo tvarka.
 
-SELECT customer_id, amount,
-	IF (amount>10,'Virš 10','Iki 10') as ar_brangiai
+select customer_id, amount,
+	if (amount>10,'virš 10','iki 10') as ar_brangiai
 	from payment
-	WHERE amount>0
-	order by amount DESC ;
+	where amount>0
+	order by amount desc ;
 
 -- Pateikite klientų sąrašą (lentelė payment) su mokėjimo data 
--- ir didžiausiu kiekvieno kliento mokėjimu, 
+-- ir didžiausiu kiekvieno kliento mokėjimu [tą dieną], 
 -- bet tik tų klientų, kurių didžiausias mokėjimas tą dieną yra šiame sąraše: 2.99, 3.99 ir 4.99.
--- Galima naudoti: in, having, date(), max()
+-- Rekomenduojama naudoti: having, in(), date(), max()
 
-SELECT date(payment_date), customer_id, MAX(amount) as did 
+select customer_id, date(payment_date), max(amount) as didelis
 	from payment
 	group by date(payment_date), customer_id
-	having did IN( 2.99, 3.99, 4.99 );
+	having didelis in( 2.99, 3.99, 4.99 )
+	order by payment_date, customer_id;
 
