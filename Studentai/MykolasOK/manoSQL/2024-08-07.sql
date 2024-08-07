202afb8b863c623c48f9cf42c14b494043aebabe
Mykolas OK <mykolasok@gmail.com>
2024-08-07 21:27:20 +0300

    Baigėme dirbti su db „sakila“.

--- Studentai/MykolasOK/manoSQL/2024-08-07.sql
+++ Studentai/MykolasOK/manoSQL/2024-08-07.sql
@@ -99,4 +99,91 @@ select	customer.first_name, customer.last_name,
 	left join payment using(customer_id)
 	group by customer.customer_id;
 
-------------------- -------------------
+------------------- Paskaitos II dalis -------------------
+
+select * from actor order by last_name; -- 200 rows.
+
+select * from ( select * from actor ) as t;
+
+select * from ( select * from actor order by last_name limit 10) as t limit 3;
+
+create table Klientai_MOK
+(
+	id int auto_increment not null primary key,
+	vardas char(255),
+	pavarde char(255)
+);
+
+drop table Klientai_MOK;
+
+insert Klientai_MOK(vardas,pavarde)
+	values('Asta','Viršulytė');
+insert Klientai_MOK(id,vardas,pavarde)
+	values(null,'Goda','Godaitė');
+insert Klientai_MOK(vardas,pavarde)
+	values('Trečiasis','Trečiokas');
+insert Klientai_MOK(id,vardas,pavarde)
+	values(1144,'Telefonas','Petrauskas');
+
+select * from Klientai_MOK;
+
+-- „sakila.ats_mok“
+
+select name, avg(length), avg(rental_duration) from film
+join film_category using(film_id)
+join category using(category_id)
+group by film_id;
+
+create temporary table ats_mok
+select name, avg(length), avg(rental_duration) from film
+join film_category using(film_id)
+join category using(category_id)
+group by film_id;
+
+select * from ats_mok;
+
+delete from ats_mok;
+drop table ats_mok;
+
+-- „sakila.Klientai_MOK“
+
+alter table Klientai_MOK
+add atnaujinta datetime;
+
+update Klientai_MOK
+	set atnaujinta=now();
+ 
+insert Klientai_MOK(vardas,pavarde,atnaujinta)
+	values('Eglė','Sesė',now());
+insert Klientai_MOK(id,vardas,pavarde,atnaujinta)
+	values(null,'Marija','Sesė',now());
+
+select * from Klientai_MOK;
+
+-- Užduotis („sakila.Klientai_temp_MOK“):
+
+create temporary table Klientai_temp_MOK
+(
+	id int not null primary key,
+	vardas varchar(255),
+	kompanija varchar(100)
+);
+
+create temporary table Klientai_tmp_MOK
+	select * from customer;
+
+select * from Klientai_tmp_MOK;
+
+update Klientai_tmp_MOK
+	set email='linda.williams@gmail.com'
+	where first_name like 'Linda' and last_name like 'Williams';
+update Klientai_tmp_MOK
+	set address_id=21
+	where first_name like 'Jennifer' and last_name like 'Davis';
+delete from Klientai_tmp_MOK
+	where first_name like 'Mary' and last_name like 'Smith';
+delete from Klientai_tmp_MOK
+	where customer_id between 4 and 6;
+
+select * from Klientai_tmp_MOK;
+
