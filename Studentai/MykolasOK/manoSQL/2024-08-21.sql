8888e17304fb1519d5e780dd36102269a376a1c4
Mykolas OK <mykolasok@gmail.com>
2024-08-21 21:41:32 +0300

    Top 5 gamintojų kainų pasiskirstymas Autoplius.

--- Studentai/MykolasOK/manoSQL/2024-08-21.sql
+++ Studentai/MykolasOK/manoSQL/2024-08-21.sql
@@ -1,2 +1,41 @@
 select lytis, GROUP_CONCAT(bdu_spalio)
-from DU2014N group by lytis;
+from DUS2014N du14 group by lytis;
+
+SELECT gamintojas, count(*) as n, GROUP_CONCAT(price) AS p
+FROM autopliuslt 
+GROUP BY gamintojas;
+
+SELECT gamintojas, count(*) n, GROUP_CONCAT(cast(replace(price,' ','') as float)) kainos
+FROM works.autopliuslt 
+GROUP BY gamintojas
+order by n desc limit 5;
+
+-- Besikartojančių ID eliminavimas panaudojant ROW_NUMBER()
+WITH T1 AS
+(SELECT gamintojas, modelis, id, price, ROW_NUMBER() OVER (PARTITION BY id) AS rn FROM autopliuslt)
+SELECT * FROM T1
+WHERE rn = 1
+ORDER BY id ASC;
+
+SELECT * FROM
+(SELECT gamintojas, modelis, id, price, ROW_NUMBER() OVER (PARTITION BY id) AS rn FROM autopliuslt)
+as T1
+WHERE rn=1;
+
+SELECT gamintojas, count(*) n, 
+	GROUP_CONCAT(cast(replace(price,' ','') as float)) kainos
+FROM (
+	SELECT gamintojas, modelis, id, price, 
+		ROW_NUMBER() OVER (PARTITION BY id) AS rn FROM autopliuslt
+) as T1
+WHERE rn=1
+GROUP BY gamintojas
+order by n desc limit 5;
+
+-- Vidutinė kaina:
+SELECT avg(cast(replace(price,' ','') as float)) vidKaina
+FROM (
+	SELECT id, price, 
+		ROW_NUMBER() OVER (PARTITION BY id) AS rn FROM works.autopliuslt
+) as T1
+WHERE rn=1;
