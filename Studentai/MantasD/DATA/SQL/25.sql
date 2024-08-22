51aa498dc244db67d242150f0d85d183ddc244d7
yglas <mantas.dzimidas@gmail.com>
2024-08-22 21:03:44 +0300

    25

---
+++ Studentai/MantasD/DATA/SQL/25.sql
@@ -0,0 +1,47 @@
+-- 1) su plot() nubraižykite BrentOilPrices istorinius duomenis. Duomenis imkite iš SQL lentelės BrentOilPrices.
+select * from BrentOilPrices;
+
+-- 2) Pridėkite vartotojo įvestį, kad su input() būtų galima nurodyti metus (nuo, iki), kurių istorinius duomenis norite pamatyti.
+select * from BrentOilPrices
+where str_to_date(Date,'%d-%M-%y') between '2001-01-01' and '2002-01-01';
+
+-- # EismoĮvykiai - kurį mėnesį įvyko daugiausiai eismo įvykių? Pateikite eismo įvykių kiekius su bar arba barh.
+select count(*), substring(dataLaikas,6,2) as men from EismIvyk2021
+group by men;
+-- destytojo
+select count(*), month(cast(dataLaikas as datetime)) as M from EismIvyk2021
+group by M;
+-- # kuriomis valandomis? Taip pat vizuallizuokite tai su bar arba barh. Stulpelis dataLaikas.
+select *,count(*), substring(dataLaikas,12,2) as men from EismIvyk2021
+group by men;
+-- destytojo
+select count(*), hour(cast(dataLaikas as datetime)) as H from EismIvyk2021
+group by H;
+-- # kokie top 5 adrresai, kuriuose yra daugiausiai eimso įvykių?
+select ivykioVieta, count(*) as r from EismIvyk2021
+group by ivykioVieta
+order by r desc limit 5 ;
+
+select count(*) from EismIvyk2021;
+-- # kiek procentų nuo visų įvykių įvyko šiuose 5kiuose adresuose?
+-- #procentinę vizulizaciją pateikite su pie plot.
+select 
+case
+when ivykioVieta in (select * from(select ivykioVieta from EismIvyk2021 group by ivykioVieta order by count(*) desc limit 5)as f) then ivykioVieta
+else 'Kitos'
+end as Vieta, count(*) as r
+from EismIvyk2021
+group by Vieta;
+
+-- # Suraskite, kuriuose rajonuose įvyksta daugiausiai susišaudymų? Vizualizuokite. nypd
+select BORO, count(*) from nypd
+group by BORO
+order BY count(*) desc;
+-- # suraskite, kuriomis valandomis įvyksta daugiausiai susišaudymų? Vizualizuokite.
+select hour(cast(OCCUR_TIME as time)) as t, count(*) from nypd
+group by t order by count(*) desc;
+-- # Kuriomis valandomis (procentiškai) įvyksta daugiausiai mirtinų susišaudymų? Vizualizuokite.
+select hour(cast(OCCUR_TIME as time)) as t, count(*) from nypd
+where STATISTICAL_MURDER_FLAG = 1
+group by t order by count(*) desc;
+
