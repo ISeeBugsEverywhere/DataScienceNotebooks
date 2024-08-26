7c2fb05eb5b77f765c0787e8841a39d1b4695831
Mykolas OK <mykolasok@gmail.com>
2024-08-26 20:02:46 +0300

    class SolarAnalyzer(): perskaito failą, bet dar neanalizuoja duomenų.

--- Studentai/MykolasOK/manoFunkcijos.py
+++ Studentai/MykolasOK/manoFunkcijos.py
@@ -14,4 +14,29 @@ def pout(a, limit=5):
 class SolarAnalyzer():
     def __init__(self,failas,sep=';',header=1):
         self.failas=failas
-        print('SolarAnalyzer. Skaitomas failas:',failas)
+        self.U = []
+        self.I = []
+        self.j = []
+        self.P = []
+        print('SolarAnalyzer.\nSkaitomas failas:',failas)
+        failas = open(failas,mode='r') # Duomenų failas.
+        for i in range(0,header):
+            print('Praleista:',failas.readline(),end='')
+        for eil in failas:
+            emas=eil.split(sep)
+            # print(emas)
+            try:
+                self.U.append(float(emas[0]))
+                self.I.append(float(emas[1]))
+                self.j.append(float(emas[2]))
+                self.P.append(float(emas[3]))
+            except ValueError:
+                print('Nepavyko atpažinti:',eil)
+        failas.close()
+        # print(self.U)
+        # print(self.I)
+        # print(self.j)
+        # print(self.P)        
+        print('SolarAnalyzer() failas perskaitytas.')
+
+
