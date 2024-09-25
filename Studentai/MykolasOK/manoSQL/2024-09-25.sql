a77339fbefffbd576c69525b19998dbea4931c65
Mykolas OK <mykolasok@gmail.com>
2024-09-25 19:56:06 +0300

    https://data.gov.lt/

--- Studentai/MykolasOK/manoSQL/2024-09-25.sql
+++ Studentai/MykolasOK/manoSQL/2024-09-25.sql
@@ -1,58 +1,18 @@
+SELECT gamintojas, count(*) n, GROUP_CONCAT(cast(replace(price,' ','') as float)) kainos
+    FROM works.autopliuslt 
+    GROUP BY gamintojas
+    order by n desc limit 10;
 
-(select avg(du.bdu_spalio) viddu, p.Kodas, p.Profesija, 'Didžiausi' tmp
-from DUS2014N du
-left join profesijos p
-on du.profesija=p.Kodas
-group by p.Kodas
-order by viddu desc
-limit 3)
-	union all
-(select avg(du.bdu_spalio) viddu, p.Kodas, p.Profesija,
-abs(avg(du.bdu_spalio)-(select avg(du.bdu_spalio) from DUS2014N du)) skirtumas
-from DU8S2014N du
-left join profesijos p
-on du.profesija=p.Kodas
-group by p.Kodas
-order by skirtumas asc
-limit 3)
-	union all
-(select avg(du.bdu_spalio) viddu, p.Kodas, p.Profesija, 'Mažiausi' tmp
-from DUS2014N du
-left join profesijos p
-on du.profesija=p.Kodas
-group by p.Kodas
-order by viddu asc
-limit 3);
+# 2025-09-25 pirmosios užduoties pirmoji dalis - duomenų gavimas:
+# Atrinkti 10 populiariausių gamintojų iš autopliuslt, 
+# pateikti tokius stulpelius - gamintojas, vidutinė kaina, vidutinė rida, vidutinis amžius, modelių kiekis. 
 
--- Pokytis 2014..2018:
-
-select * from 
-(select * from 
-(select avg(du.bdu_spalio) viddu, p.Kodas, p.Profesija, 'Didžiausi'
-from DUS2014N du
-left join profesijos p
-on du.profesija=p.Kodas
-group by p.Kodas
-order by viddu desc
-limit 5) as t1
-	union all
-select * from
-(select avg(du.bdu_spalio) viddu, p.Kodas, p.Profesija,
-abs(avg(du.bdu_spalio)-(select avg(du.bdu_spalio) from DUS2014N du)) skirtumas
-from DUS2014N du
-left join profesijos p
-on du.profesija=p.Kodas
-group by p.Kodas
-order by skirtumas asc
-limit 5) as t2
-	union all
-select * from
-(select avg(du.bdu_spalio) viddu, p.Kodas, p.Profesija, 'Mažiausi'
-from DUS2014N du
-left join profesijos p
-on du.profesija=p.Kodas
-group by p.Kodas
-order by viddu asc
-limit 5) as t3) as du14
-left join DUS2018N du18 on du18.profesija=du14.Kodas
-order by viddu asc;
+SELECT 	gamintojas, 
+		ROUND(AVG(cast(replace(price,' ','') as float))) kaina,
+		ROUND(AVG(cast(replace(replace(rida,' ',''),'km','') as float))) r,
+		ROUND(AVG(cast(pagaminimo_data) as float)) data,
+		pagaminimo_data,
+		count(*) n
+    FROM works.autopliuslt 
+    GROUP BY gamintojas
+    order by n desc limit 10;
