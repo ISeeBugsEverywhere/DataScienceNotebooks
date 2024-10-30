78e935f9a1189e77703ba56b089de21aa349c22c
ISeeBugsEverywhere <menulio.svytuokle@gmail.com>
2024-10-30 15:55:34 +0200

    upd

---
+++ Notebooks/Testavimas/type_annot.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+
+# from typing import * #negražu, bet susikeliame visus tipus
+#  visgi geriau
+import typing as T
+
+#  rašome funkciją:
+def addDigits(x:float, y:float) -> float:
+    r:float = x + y
+    return r
+
+ats = addDigits(1,2)
+print(ats)
+ats = addDigits('2','2')
+print(ats)
+
+def so_text(text:str):
+    text.capitalize()
+    pass
+
+def doGoodWork(df:pd.DataFrame) -> np.ndarray:
+    df.
+    pass
