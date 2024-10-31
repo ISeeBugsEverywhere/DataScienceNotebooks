3d1441fbc370ac90281b368388cfd42f86d705c7
Mykolas OK <mykolasok@gmail.com>
2024-11-01 00:09:14 +0200

    Atlikto užduotys + palyginti Fibonači metodai.

--- Studentai/MykolasOK/2024-10-31_duom_tipai.py
+++ Studentai/MykolasOK/2024-10-31_duom_tipai.py
@@ -29,3 +29,23 @@ sfunkcija('abc')
 # 2024-10-31_duom_tipai.py:10: error: Unsupported operand types for + ("int" and "str")  [operator]
 # 2024-10-31_duom_tipai.py:10: note: Both left and right operands are unions
 # Found 2 errors in 1 file (checked 1 source file)
+
+import doctest
+ 
+def add(x, y):
+    '''sums up two digits
+    >>> add(1,2)
+    3
+    >>> add(5,-4)
+    1
+    >>> add(0,0)
+    0
+    >>> add(5,'5')
+    Traceback (most recent call last):
+    TypeError: unsupported operand type(s) for +: 'int' and 'str'
+    '''
+    return x + y
+
+if __name__ == '__main__':
+    doctest.testmod(verbose=True, globs=globals(), report=True)
+
