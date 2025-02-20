d902c99478b29cdbad75ebafd85025305ded814f
yglas <mantas.dzimidas@gmail.com>
2025-02-20 20:39:58 +0200

    93

---
+++ Studentai/MantasD/Skriptai/dbscan-kmeans.py
@@ -0,0 +1,15 @@
+# 1: Klasterizuokite pirkėjus (k-Means, DBSCAN)
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("vjchoudhary7/customer-segmentation-tutorial-in-python")
+
+print("Path to dataset files:", path)
+
+# 2: Parašykite kodą, pagal parametrus nustatantį kokybę (quality)
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("uciml/red-wine-quality-cortez-et-al-2009")
+
+print("Path to dataset files:", path)
